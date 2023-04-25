1bf13048f70484ccbb1712b86f8b5e263aff9565
dathoang997 <dathoang997@gmail.com>
2023-04-25 14:14:33 +0700

    get arb airdrop wallet

---
+++ service/arb_airdrop.js
@@ -0,0 +1,53 @@
+const fetch = require("node-fetch");
+const range = 1000
+fs = require("fs");
+
+const getWallet = async () => {
+  await getApi(0, 84064042); //26024907
+};
+
+const getApi = async (fromBlock, toBlock) => {
+  const url = `https://api.arbiscan.io/api?module=logs&action=getLogs&fromBlock=${fromBlock}&toBlock=${toBlock}&address=0x67a24ce4321ab3af51c2d0a4801c3e111d88c9d9&topic0=0x87aeeb9eda09a064caef63d00f62c15063631980bfc422ad7dd30c8a79f0cbb7&apikey=G4UZWDHIR33H59QNXBQF8IR6XQSD521BGV`;
+  const response = await fetch(url);
+  console.log(url);
+  try {
+    const body = JSON.parse(await response.text());
+    console.log(body.result.length);
+    const lastBlock = parseInt(
+      body.result[body.result.length - 1].blockNumber,
+      16,
+    );
+    console.log(lastBlock);
+    await saveData(body.result, lastBlock, toBlock);
+  } catch (error) {
+    console.log(error, { fromBlock, toBlock });
+    getApi(fromBlock, toBlock);
+  }
+};
+
+saveData = async (data, lastBlock, toBlock) => {
+  console.log("saving data...");
+  let savingData = []
+  let continueBlock = parseInt(data[data.length - 1].blockNumber) + 1;
+  for (let i = 0; i < data.length - 1; i++) {
+    if (data.length == range && data[i].blockNumber == lastBlock) {
+      continueBlock = parseInt(data[data.length - 1].blockNumber);
+      continue;
+    }
+    const address = data[i].topics[1].slice(0,2) + data[i].topics[1].slice(25)
+    const amount = parseInt(data[i].data, 16)
+    console.log({address, amount})
+    // savingData.push({address, amount})
+  }
+  let config = JSON.stringify(savingData);
+  await fs.writeFileSync("arb.json", config, (error) => {});
+  console.log("data saved!");
+  if (data.length < range) {
+    process.exit();
+  }
+  console.log("continue form ", continueBlock);
+  getApi(continueBlock, toBlock);
+};
+module.exports = {
+  getWallet,
+};
